a1492add2ffa4190ac4d196d89e580fc340792e5
DdooBuggy <yhiyo03@gmail.com>
2022-04-28 21:48:04 +0900

    ~#4.12

--- src/index.tsx
+++ src/index.tsx
@@ -1,11 +1,16 @@
 import { createRoot } from "react-dom/client";
 import App from "./App";
+import { QueryClient, QueryClientProvider } from "react-query";
 import { ThemeProvider } from "styled-components";
 import { theme } from "./theme";
 
+const queryClient = new QueryClient();
+
 const root = createRoot(document.getElementById("root")!);
 root.render(
-  <ThemeProvider theme={theme}>
-    <App />
-  </ThemeProvider>
+  <QueryClientProvider client={queryClient}>
+    <ThemeProvider theme={theme}>
+      <App />
+    </ThemeProvider>
+  </QueryClientProvider>
 );
--- src/routes/Coin.tsx
+++ src/routes/Coin.tsx
@@ -1,7 +1,8 @@
-import { useEffect, useState } from "react";
 import { useLocation, useParams } from "react-router";
-import { Outlet } from "react-router-dom";
+import { Outlet, useMatch, Link } from "react-router-dom";
 import styled from "styled-components";
+import { useQuery } from "react-query";
+import { fetchCoinInfo, fetchCoinTickers } from "../api";
 
 // components
 const Title = styled.h1`
@@ -27,6 +28,29 @@ const Header = styled.header`
   align-items: center;
 `;
 
+const Tabs = styled.div`
+  display: grid;
+  grid-template-columns: repeat(2, 1fr);
+  margin: 25px 0px;
+  gap: 10px;
+`;
+
+const Tab = styled.span<{ isActive: boolean }>`
+  text-align: center;
+  text-transform: uppercase;
+  font-size: 12px;
+  font-weight: 400;
+  background-color: rgba(0, 0, 0, 0.5);
+  padding: 7px 0px;
+  border-radius: 10px;
+  color: ${(props) =>
+    props.isActive ? props.theme.accentColor : props.theme.textColor};
+  a {
+    display: block;
+    padding: 7px 0px;
+  }
+`;
+
 // style
 const Overview = styled.div`
   display: flex;
@@ -39,6 +63,7 @@ const OverviewItem = styled.div`
   display: flex;
   flex-direction: column;
   align-items: center;
+  width: 33%;
   span:first-child {
     font-size: 10px;
     font-weight: 400;
@@ -112,28 +137,25 @@ interface RouteState {
 
 // function
 function Coin() {
-  const [loading, setLoading] = useState(true);
   const { coinId } = useParams<{ coinId: string }>();
   const name = (useLocation().state as RouteState)?.name;
-  const [info, setInfo] = useState<InfoData>();
-  const [priceInfo, setPriceInfo] = useState<PiceData>();
-  useEffect(() => {
-    (async () => {
-      const infoData = await (
-        await fetch(`https://api.coinpaprika.com/v1/coins/${coinId}`)
-      ).json();
-      setInfo(infoData);
-      const priceData = await (
-        await fetch(`https://api.coinpaprika.com/v1/tickers/${coinId}`)
-      ).json();
-      setPriceInfo(priceData);
-      setLoading(false);
-    })();
-  }, [coinId]);
+  const priceMatch = useMatch("/:coinId/price");
+  const chartMatch = useMatch("/:coinId/chart");
+
+  const { isLoading: infoLoading, data: infoData } = useQuery<InfoData>(
+    ["info", coinId],
+    () => fetchCoinInfo(coinId ? coinId : "")
+  );
+  const { isLoading: tickersLoading, data: tickersData } = useQuery<PiceData>(
+    ["tickers", coinId],
+    () => fetchCoinInfo(coinId ? coinId : "")
+  );
+  const loading = infoLoading || tickersLoading;
+
   return (
     <Container>
       <Header>
-        <Title>{name ? name : loading ? "Loading..." : info?.name}</Title>
+        <Title>{name ? name : loading ? "Loading..." : infoData?.name}</Title>
       </Header>
       {loading ? (
         <Loader>Loading...</Loader>
@@ -142,29 +164,37 @@ function Coin() {
           <Overview>
             <OverviewItem>
               <span>Rank:</span>
-              <span>{info?.rank}</span>
+              <span>{infoData?.rank}</span>
             </OverviewItem>
             <OverviewItem>
               <span>Symbol:</span>
-              <span>${info?.symbol}</span>
+              <span>${infoData?.symbol}</span>
             </OverviewItem>
             <OverviewItem>
               <span>Open Source:</span>
-              <span>{info?.open_source ? "Yes" : "No"}</span>
+              <span>{infoData?.open_source ? "Yes" : "No"}</span>
             </OverviewItem>
           </Overview>
-          <Description>{info?.description}</Description>
+          <Description>{infoData?.description}</Description>
           <Overview>
             <OverviewItem>
               <span>Total Suply:</span>
-              <span>{priceInfo?.total_supply}</span>
+              <span>{tickersData?.total_supply}</span>
             </OverviewItem>
             <OverviewItem>
               <span>Max Supply:</span>
-              <span>{priceInfo?.max_supply}</span>
+              <span>{tickersData?.max_supply}</span>
             </OverviewItem>
           </Overview>
-          <Outlet />
+          <Tabs>
+            <Tab isActive={chartMatch !== null}>
+              <Link to={`/${coinId}/chart`}>Chart</Link>
+            </Tab>
+            <Tab isActive={priceMatch !== null}>
+              <Link to={`/${coinId}/price`}>Price</Link>
+            </Tab>
+          </Tabs>
+          <Outlet context={{ coinId: coinId }} />
         </>
       )}
     </Container>
