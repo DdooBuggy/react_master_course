4411a27e92ec0c6423f1a31fd0cb8a4261758a6c
DdooBuggy <yhiyo03@gmail.com>
2022-05-04 20:48:05 +0900

    ~#5.12

--- src/index.tsx
+++ src/index.tsx
@@ -2,10 +2,13 @@ import { createRoot } from "react-dom/client";
 import App from "./App";
 import { ThemeProvider } from "styled-components";
 import { theme } from "./theme";
+import { RecoilRoot } from "recoil";
 
 const root = createRoot(document.getElementById("root")!);
 root.render(
-  <ThemeProvider theme={theme}>
-    <App />
-  </ThemeProvider>
+  <RecoilRoot>
+    <ThemeProvider theme={theme}>
+      <App />
+    </ThemeProvider>
+  </RecoilRoot>
 );
